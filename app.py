15f2abc2e9274e848fd4c2e7a17f7220c4b30c26
ashford <ashford.fernandes1111998@gmail.com>
2024-09-24 19:39:19 +0100

    added sqlintegration

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://portfolio_db_hrjv_user:Rh2vNxRgrxN1EGMIGA8FAk0oDJomJ6kA@dpg-crpfprm8ii6s73chagng-a.oregon-postgres.render.com/portfolio_db_hrjv"
+# initialize the app with the extension
+db.init_app(app)
+
+class Messages(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str]
+    message: Mapped[str]
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
@@ -14,9 +36,19 @@ def about():
 def contact():
     if request.method == 'POST':
         name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']   
+        new_message = Messages(name=name, email=email, message=message)
+        db.session.add(new_message)
+        db.session.commit()
         return render_template("contact-success.html", name=name)
 
     return render_template("contact.html")
+
+@app.route("/admin")
+def admin():
+    messages = db.session.execute(db.select(Messages).order_by(Messages.id)).scalars()
+    return render_template("admin.html", messages=messages)
 
 @app.route("/projects")
 def projects():
